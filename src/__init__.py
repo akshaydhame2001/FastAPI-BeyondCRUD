2bd479c12f744e68af3b773a6057877fa61c573a
akshaydhame2001 <akshaydhame2001@gmail.com>
2025-03-11 10:44:19 +0530

    commit

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
 from contextlib import asynccontextmanager
+from src.db.main import init_db
 
 @asynccontextmanager
 async def life_span(app:FastAPI):
     print(f"Server is starting ... ")
+    await init_db()
     yield
     print(f"Server has been stopped")
 
