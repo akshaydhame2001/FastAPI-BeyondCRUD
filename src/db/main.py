2ce05d2d92f529549e2cac03c6a530d5b35acb75
akshaydhame2001 <akshaydhame2001@gmail.com>
2025-03-11 16:11:26 +0530

    commit

--- src/db/main.py
+++ src/db/main.py
@@ -1,5 +1,6 @@
-from sqlmodel import create_engine, text
+from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
+
 from src.config import Config
 
 engine = AsyncEngine(
@@ -10,8 +11,6 @@ engine = AsyncEngine(
 
 async def init_db():
     async with engine.begin() as conn:
-        statement = text("SELECT 'hello';")
+        from src.books.models import Book
 
-        result = await conn.execute(statement)
-
-        print(result.all())
+        await conn.run_sync(SQLModel.metadata.create_all)
