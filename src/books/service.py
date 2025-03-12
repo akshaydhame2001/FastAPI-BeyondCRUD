5775ab1a8fc6458716bac4e9299c50fb0e6be08e
akshaydhame2001 <akshaydhame2001@gmail.com>
2025-03-12 12:41:38 +0530

    commit

--- src/books/service.py
+++ src/books/service.py
@@ -2,6 +2,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from .schemas import BookCreateModel, BookUpdateModel
 from sqlmodel import select, desc
 from .models import Book
+from datetime import datetime
 
 class BookService:
     async def get_all_books(self, session: AsyncSession):
@@ -27,6 +28,8 @@ class BookService:
             **book_data_dict # unpacking
         )
 
+        new_book.published_date = datetime.strptime(book_data_dict['published_date'], "%Y-%m-%d")
+
         session.add(new_book)
 
         await session.commit()
@@ -34,7 +37,7 @@ class BookService:
         return new_book
 
     async def update_book(self, book_uid: str, update_data: BookUpdateModel, session: AsyncSession):
-        book_to_update = self.get_book(book_uid, session)
+        book_to_update = await self.get_book(book_uid, session)
 
         if book_to_update is not None:
             update_data_dict = update_data.model_dump()
@@ -50,7 +53,7 @@ class BookService:
             return None
 
     async def delete_book(self, book_uid: str, session: AsyncSession):
-        book_to_delete = self.get_book(book_uid, session)
+        book_to_delete = await self.get_book(book_uid, session)
 
         if book_to_delete is not None:
             await session.delete(book_to_delete)
