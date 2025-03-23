a83c9872d6e86d0fa64e0a8ecf812cce66023a5d
akshaydhame2001 <akshaydhame2001@gmail.com>
2025-03-23 21:34:19 +0530

    commit

---
+++ src/auth/dependencies.py
@@ -0,0 +1,11 @@
+from fastapi import Request
+from fastapi.security import HTTPBearer
+from fastapi.security.http import HTTPAuthorizationCredentials
+
+class AccessTokenBearer(HTTPBearer):
+    
+    def __init__(self, auto_error = True):
+        super().__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
+        creds =  await super().__call__(request)
