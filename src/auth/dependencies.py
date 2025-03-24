59fe9eb22a7885f227219dc4387cb8ae9b4ae2b3
akshaydhame2001 <akshaydhame2001@gmail.com>
2025-03-24 18:43:48 +0530

    HTTP Bearer Auth

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -1,6 +1,8 @@
-from fastapi import Request
+from fastapi import Request, status
 from fastapi.security import HTTPBearer
 from fastapi.security.http import HTTPAuthorizationCredentials
+from .utils import decode_token
+from fastapi.exceptions import HTTPException
 
 class AccessTokenBearer(HTTPBearer):
     
@@ -9,3 +11,27 @@ class AccessTokenBearer(HTTPBearer):
 
     async def __call__(self, request: Request) -> HTTPAuthorizationCredentials | None:
         creds =  await super().__call__(request)
+
+        token = creds.credentials
+
+        token_data = decode_token(token)
+
+        if not self.token_valid:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid or expired token"
+            )
+
+        if token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Please provide an access token"
+            )
+
+        return token_data
+
+    def token_valid(self, token, str) -> bool:
+
+        token_data = decode_token(token)
+
+        return True if token_data is not None else False
